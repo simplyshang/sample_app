29262024100f4fc86234901ebf880c5ab4b8a62c
Shannon Gray <shannongray@gmail.com>
2012-03-17 01:59:43 -0400

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name                  "Shannon Gray"
+	user.email                 "shannon@example.com"
+	user.password              "foobar"
+	user.password_confirmation "foobar"
+end
